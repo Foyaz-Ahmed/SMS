e4267e70eb51c3eb5c5adc4e7c0baced302360cd
Foyaz-Ahmed <ahmedfoyaz0000@gmail.com>
2021-12-28 21:32:32 +0600

    Layerd Architecture

---
+++ SMS.ApplicationService/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.ApplicationService
+{
+    public class Class1
+    {
+    }
+}
